03192d5c53350069090ab2d5ce103953452d8fa8
ThinhDDP <thinhdoducphuc@gmail.com>
2021-03-25 22:31:51 +0700

    Added Responsive (Poor)

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 if (temp = JSON.parse(localStorage.getItem('CurrentUser')), temp != null){
     document.getElementById("status").removeAttribute("href")
     document.getElementById("status").innerHTML = "Xin chào " + String(temp);
-    document.getElementById("status").style.marginLeft = "500px";
     document.getElementById("status").style.textDecoration = "none"
 }
